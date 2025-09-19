c3145ec013b3f5a2902e6a0a9b4c322647ab5a6c
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-09-19 16:20:38 +0500

    Update AdminRoute.jsx

--- frontend/src/pages/Admin/AdminRoute.jsx
+++ frontend/src/pages/Admin/AdminRoute.jsx
@@ -1,13 +1,3 @@
-// import React from 'react'
-// import { Navigate,Outlet } from 'react-router-dom'
-// import { useSelector } from 'react-redux'
-// const AdminRoute = () => {
-//     const {userInfo}=useSelector(state=>state.auth)
-//   return userInfo && userInfo.isAdmin?(<Outlet/>):(<Navigate to='/login' replace/>)
-// }
-
-// export default AdminRoute
-
 
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
@@ -17,9 +7,9 @@ const AdminRoute = () => {
   const { userInfo } = useSelector((state) => state.auth)
 
   if (!userInfo) return <Navigate to="/login" replace />
+  
   if (!userInfo.isAdmin) return <Navigate to="/unauthorized" replace />
 
-  // ✅ This is the key part to render nested routes inside AdminRoute
   return <Outlet />
 }
 
