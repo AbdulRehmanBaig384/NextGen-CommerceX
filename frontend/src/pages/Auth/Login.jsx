5c8ee00e6bfbe47d37cb8e3c454ba2f6595806fd
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-09-20 10:38:46 +0500

    Update Login.jsx

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -53,21 +53,6 @@
 //               {isLoading && <Loader/>}
 //             </form>
 
-//             <div className='mt-4' >
-//               <p className='text-white '>
-//                 New Customer ?{" "}<Link to={redirect ?`/register?redirect=${redirect}`:`/register`} className='text-pink-500 hover:underline'>Register Here</Link>
-//               </p>
-//             </div>
-//         </div>
-//         <img  src="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80" alt='test' className='h-[30rem] w-[30%] xl:block md:hidden sm:hidden rounded-lg'></img>
-//       </section>
-//     </div>
-//   )
-// }
-
-// export default Login
-
-
 
 
 
