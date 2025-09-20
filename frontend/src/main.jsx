ea51810ae2df60a0a9f06bb071ccc79c61c5f84f
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-09-20 10:45:05 +0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -50,30 +50,6 @@ const router=createBrowserRouter(
     )
 )
 
-
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path='/' element={<App />}>
-//       <Route path='login' element={<Login />} />
-//       <Route path='register' element={<Register />} />
-
-//       <Route path='' element={<PrivateRoute />}>
-//         <Route path='profile' element={<Profile />} />
-//       </Route>
-
-//       <Route path='admin' element={<AdminRoute />}>
-//         <Route path='userlist' element={<UserList />} />
-//         <Route path='categorylist' element={<CategoryList />} />
-//       </Route>
-//     </Route>
-//   )
-// )
-
-
-
-
-
-
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <RouterProvider router={router} />
